c95a940ee15f24113215bc351c06373dd3cfedd1
keyrelations <keyrelations@gmail.com>
2016-03-01 15:12:35 +0530

    change

--- app/src/main/java/com/keyrelations/suggestmesomemovies/MainActivity.java
+++ app/src/main/java/com/keyrelations/suggestmesomemovies/MainActivity.java
@@ -1,19 +1,80 @@
 package com.keyrelations.suggestmesomemovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initialise a call back manager to handle the callback from login button
+    CallbackManager callbackManager = CallbackManager.Factory.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //initialise the facebook sdk
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext(), new FacebookSdk.InitializeCallback() {
+            @Override
+            public void onInitialized() {
+                // if access token already exist and valid
+                if (AccessToken.getCurrentAccessToken() != null && !AccessToken.getCurrentAccessToken().isExpired()) {
+                    navigateToHomeActivity();
+                }
+            }
+        });
 
         //sets the layout file
         setContentView(R.layout.activity_main);
+
+        //create a login button instance
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        //save the needed permissions in an array
+        List<String> permissionNeeds = Arrays.asList("public_profile");
+
+        //set the needed permissions from array
+        loginButton.setReadPermissions(permissionNeeds);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // Code for login success
+                navigateToHomeActivity();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void navigateToHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
