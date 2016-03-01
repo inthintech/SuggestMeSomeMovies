6b883b7d1f036759647fcb9fde99050ffcaf5411
keyrelations <keyrelations@gmail.com>
2016-03-01 11:31:55 +0530

    change

---
+++ app/src/main/java/com/keyrelations/suggestmesomemovies/MainActivity.java
@@ -0,0 +1,14 @@
+package com.keyrelations.suggestmesomemovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //sets the layout
+        setContentView(R.layout.activity_main);
+    }
+}
