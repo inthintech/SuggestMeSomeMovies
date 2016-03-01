4fec9a6a5dc3c5bef21237fe08fc461a327f7190
keyrelations <keyrelations@gmail.com>
2016-03-01 11:42:05 +0530

    change

--- app/src/main/java/com/keyrelations/suggestmesomemovies/MainActivity.java
+++ app/src/main/java/com/keyrelations/suggestmesomemovies/MainActivity.java
@@ -2,12 +2,17 @@ package com.keyrelations.suggestmesomemovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //initialise the facebook sdk
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         //sets the layout file
         setContentView(R.layout.activity_main);
     }
